fd328771df045e808aa0dd75f53a5e0399f8584d
David Higgins <thehig@gmail.com>
2017-05-14 18:40:10 +0100

    Fixed all the basic linting issues. Need to reconcile the meteor imports

--- client/startup.js
+++ client/startup.js
@@ -1,11 +1,6 @@
-import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
- 
-// import amc from 'arpggio-map-component';
 
-// console.log('amc', amc);
- 
 Meteor.startup(() => {
   render(<div>App goes here</div>, document.getElementById('render-target'));
 });
