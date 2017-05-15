d9f3443e2255657396d54b633a77626aa882a671
David Higgins <thehig+github@gmail.com>
2017-05-15 13:54:41 +0100

    Properly load in the map-component (#2)

--- client/startup.js
+++ client/startup.js
@@ -1,6 +1,11 @@
+import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
+import MapComponent from 'arpggio-map-component'; //eslint-disable-line
+
+
 Meteor.startup(() => {
-  render(<div>App goes here</div>, document.getElementById('render-target'));
+  // console.log('Map Component', mapComponent);
+  render(<MapComponent />, document.getElementById('render-target'));
 });
